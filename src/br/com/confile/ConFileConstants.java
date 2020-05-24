87d980e05ab365f20a6a96e3b0b441b4972d6ecb
covadevs <covadevs@gmail.com>
2020-05-24 15:22:45 -0300

    remoção de codigo desnecessario

--- src/br/com/confile/ConFileConstants.java
+++
@@ -1,8 +0,0 @@
-package br.com.confile;
-
-public class ConFileConstants {
-
-    public static final String COMMANDS_AVAILABLE_PATTERN = "/s?|/o?|/q?|/l?|/c?|/m?|/#?|/f?|/x?";
-
-    private ConFileConstants() {}
-}
